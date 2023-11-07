e072826f97b25cfc9972ba483e965a1b5bef0879
Yurii <yurkakharitonyuk@gmail.com>
2023-11-07 19:35:58 +0200

    Miniaudio

--- src/client/main.c
+++ src/client/main.c
@@ -1,56 +1,62 @@
-//
-// Created by artur on 28.10.23.
-//
+#define MA_NO_DECODING
+#define MA_NO_ENCODING
+#define MINIAUDIO_IMPLEMENTATION
+#include "miniaudio.h"
+
 #include <stdio.h>
-#include "string_extension.h"
-#include "cross_sockets/cross_sockets.h"
-#include "cross_sockets/ip_address.h"
-#include "soundio/soundio.h"
-#include "logger/logger.h"
-#include "client/soundio_microphone.h"
-#include <pthread.h>
 
+#define DEVICE_FORMAT       ma_format_f32
+#define DEVICE_CHANNELS     2
+#define DEVICE_SAMPLE_RATE  48000
 
-int main()
+void data_callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount)
 {
-    int32_t errcode;
-
-    soundio_args_t soundio_args;
-
-    errcode = initialize_soundio(&soundio_args);
-
-    if (errcode != 0)
-    {
-        LOG_ERROR(stderr, "soundio initialization failed, error code: %d", errcode);
-        return -1;
-    }
-
-    cross_socket_initialize();
-
-    uint64_t socket = cross_socket_open_udp();
-
-    cross_socket_bind(socket, 12345);
-
-    pthread_t send;
-    thread_args_t send_args;
-    send_args.socket = socket;
-    send_args.soundio_args = &soundio_args;
-    pthread_create(&send, nullptr, send_thread_callback, (void*) &send_args);
-
-    pthread_t receive;
-    thread_args_t receive_args;
-    receive_args.socket = socket;
-    receive_args.soundio_args = &soundio_args;
-    pthread_create(&receive, nullptr, receive_thread_callback, (void*) &receive_args);
+    ma_waveform* pSineWave;
 
 
-    while(true)
-    {
+    pSineWave = (ma_waveform*)pDevice->pUserData;
 
-    }
+    ma_waveform_read_pcm_frames(pSineWave, pOutput, frameCount, NULL);
 
-
-    cross_socket_cleanup();
-    cleanup_soundio(&soundio_args);
+    (void)pInput;   /* Unused. */
 }
 
+int main(int argc, char** argv)
+{
+    ma_waveform sineWave;
+    ma_device_config deviceConfig;
+    ma_device device;
+    ma_waveform_config sineWaveConfig;
+
+    deviceConfig = ma_device_config_init(ma_device_type_playback);
+    deviceConfig.playback.format   = DEVICE_FORMAT;
+    deviceConfig.playback.channels = DEVICE_CHANNELS;
+    deviceConfig.sampleRate        = DEVICE_SAMPLE_RATE;
+    deviceConfig.dataCallback      = data_callback;
+    deviceConfig.pUserData         = &sineWave;
+
+    if (ma_device_init(NULL, &deviceConfig, &device) != MA_SUCCESS) {
+        printf("Failed to open playback device.\n");
+        return -4;
+    }
+
+    printf("Device Name: %s\n", device.playback.name);
+
+    sineWaveConfig = ma_waveform_config_init(device.playback.format, device.playback.channels, device.sampleRate, ma_waveform_type_sine, 0.2, 220);
+    ma_waveform_init(&sineWaveConfig, &sineWave);
+
+    if (ma_device_start(&device) != MA_SUCCESS) {
+        printf("Failed to start playback device.\n");
+        ma_device_uninit(&device);
+        return -5;
+    }
+
+    printf("Press Enter to quit...\n");
+    getchar();
+
+    ma_device_uninit(&device);
+
+    (void)argc;
+    (void)argv;
+    return 0;
+}
