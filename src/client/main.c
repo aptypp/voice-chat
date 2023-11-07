676740b1f0965dc0710f430c0754be53df941ace
Yurii <yurkakharitonyuk@gmail.com>
2023-11-08 00:59:19 +0200

    Added working sound

--- src/client/main.c
+++ src/client/main.c
@@ -2,34 +2,37 @@
 #include "miniaudio.h"
 #include <stdio.h>
 #include <pthread.h>
+#include <unistd.h>
 #include "cross_sockets/cross_sockets.h"
 #include "cross_sockets/ip_address.h"
 
 typedef struct {
     ma_device_config deviceConfig;
     ma_device device;
-    ma_device_data_proc callback;
     size_t ring_buffer_size_in_bytes;
     ma_rb input_ring_buffer;
     ma_rb output_ring_buffer;
 } sound_t;
 
 void sound_callback(ma_device* device, void* output, const void* input, ma_uint32 frameCount) {
-    sound_t sound = *(sound_t*)device->pUserData;
+    sound_t* sound = (sound_t*)device->pUserData;
 
+    size_t write_size_in_bytes = frameCount * ma_get_bytes_per_frame(device->capture.format, device->capture.channels);
     char* write_buffer_pointer;
+    ma_rb_acquire_write(&sound->input_ring_buffer, &write_size_in_bytes, (void*)&write_buffer_pointer);
+
+    size_t read_size_in_bytes = ma_rb_available_read(&sound->output_ring_buffer);
     char* read_buffer_pointer;
+    ma_rb_acquire_read(&sound->output_ring_buffer, &read_size_in_bytes, (void*)&read_buffer_pointer);
 
-    ma_rb_acquire_write(&sound.input_ring_buffer, &sound.ring_buffer_size_in_bytes, (void*)&write_buffer_pointer);
-    ma_rb_acquire_read(&sound.output_ring_buffer, &sound.ring_buffer_size_in_bytes, (void*)&read_buffer_pointer);
+    memcpy(write_buffer_pointer, input, write_size_in_bytes);
+    memcpy(output, read_buffer_pointer, read_size_in_bytes);
 
-    memcpy(write_buffer_pointer, input, frameCount * ma_get_bytes_per_frame(device->capture.format, device->capture.channels));
 
-    ma_uint32 available_bytes = ma_rb_available_read(&sound.output_ring_buffer);
-    memcpy(output, read_buffer_pointer, available_bytes);
+    ma_rb_commit_write(&sound->input_ring_buffer, write_size_in_bytes);
+    ma_rb_commit_read(&sound->output_ring_buffer, read_size_in_bytes);
 
-    ma_rb_commit_write(&sound.input_ring_buffer, sound.ring_buffer_size_in_bytes);
-    ma_rb_commit_read(&sound.output_ring_buffer, available_bytes);
+    fprintf(stderr, "framesCount: %d\n", frameCount);
 }
 
 void sound_initialize(sound_t* sound) {
@@ -41,10 +44,14 @@ void sound_initialize(sound_t* sound) {
     sound->deviceConfig.playback.pDeviceID = NULL;
     sound->deviceConfig.playback.format = ma_format_s16;
     sound->deviceConfig.playback.channels = 2;
-    sound->deviceConfig.dataCallback = sound->callback;
+    sound->deviceConfig.dataCallback = sound_callback;
     sound->deviceConfig.pUserData = sound;
+    sound->deviceConfig.periodSizeInFrames = 448;
+    sound->deviceConfig.sampleRate = 44100;
 
-    sound->ring_buffer_size_in_bytes = 4096 * ma_get_bytes_per_frame(sound->device.capture.format, sound->device.capture.channels);
+    ma_device_init(NULL, &sound->deviceConfig, &sound->device);
+
+    sound->ring_buffer_size_in_bytes = sound->deviceConfig.periodSizeInFrames * ma_get_bytes_per_frame(sound->device.capture.format, sound->device.capture.channels);
 
     ma_rb_init(
             sound->ring_buffer_size_in_bytes,
@@ -58,7 +65,6 @@ void sound_initialize(sound_t* sound) {
             nullptr,
             &sound->output_ring_buffer);
 
-    ma_device_init(NULL, &sound->deviceConfig, &sound->device);
     ma_device_start(&sound->device);
 }
 
@@ -68,51 +74,46 @@ void sound_cleanup(sound_t* sound) {
 
 uint64_t descriptor;
 
-#define PACKET_SIZE_IN_BYTES 1024
+#define PACKET_SIZE_IN_BYTES 448 * 4
 
 void* receive_callback(void* args) {
-    sound_t sound = *(sound_t*)args;
+    sound_t* sound = (sound_t*)args;
 
     uint32_t out_address;
     uint16_t out_port;
 
     while (true) {
+        size_t write_buffer_size = PACKET_SIZE_IN_BYTES;
+
         char* write_buffer_pointer;
-        ma_rb_acquire_write(&sound.output_ring_buffer, &sound.ring_buffer_size_in_bytes, (void*)&write_buffer_pointer);
+        ma_rb_acquire_write(&sound->output_ring_buffer, &write_buffer_size, (void*)&write_buffer_pointer);
 
         cross_socket_receive_udp(descriptor, write_buffer_pointer, PACKET_SIZE_IN_BYTES, &out_address, &out_port);
-
-        fprintf(stderr, "error: %s\n", get_error());
-        fprintf(stderr, "receive data: %s\n", write_buffer_pointer);
-
-        ma_rb_commit_write(&sound.output_ring_buffer, PACKET_SIZE_IN_BYTES);
+        ma_rb_commit_write(&sound->output_ring_buffer, PACKET_SIZE_IN_BYTES);
     }
 
     return nullptr;
 }
 
 void* send_callback(void* args) {
-    sound_t sound = *(sound_t*)args;
+    sound_t* sound = (sound_t*)args;
 
     uint32_t in_address = string_address_to_integer("127.0.0.1");
     uint16_t in_port = 12345;
 
     while (true) {
-        ma_uint32 available_bytes = ma_rb_available_read(&sound.input_ring_buffer);
+        ma_uint32 available_bytes = ma_rb_available_read(&sound->input_ring_buffer);
 
         if (available_bytes < PACKET_SIZE_IN_BYTES) {
             continue;
         }
 
         char* read_buffer_pointer;
-        ma_rb_acquire_read(&sound.input_ring_buffer, &sound.ring_buffer_size_in_bytes, (void*)&read_buffer_pointer);
+        size_t read_buffer_size = PACKET_SIZE_IN_BYTES;
+        ma_rb_acquire_read(&sound->input_ring_buffer, &read_buffer_size, (void*)&read_buffer_pointer);
 
         cross_socket_send_udp(descriptor, read_buffer_pointer, PACKET_SIZE_IN_BYTES, in_address, in_port);
-
-        fprintf(stderr, "error: %s\n", get_error());
-        fprintf(stderr, "send data: %s\n", read_buffer_pointer);
-
-        ma_rb_commit_read(&sound.input_ring_buffer, PACKET_SIZE_IN_BYTES);
+        ma_rb_commit_read(&sound->input_ring_buffer, PACKET_SIZE_IN_BYTES);
     }
 
     return nullptr;
